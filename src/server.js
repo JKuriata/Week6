3d133a81afa4a85b7797ab811ec110c1c36817a2
JKuriata <JK271096@outlook.com>
2024-02-05 14:48:01 +0000

    enhancement: example site complete

--- src/server.js
+++ src/server.js
@@ -3,10 +3,12 @@ const express = require("express");
 
 const app = express();
 
+app.use("/example", express.static("example"));
+
 // app.get("/health", (req, res) => {
 //     res.send("healthy");
 // });
 
 app.listen(5001, () => {
-    console.log("Server is listening on port 5001");
+  console.log("Server is listening on port 5001");
 });
