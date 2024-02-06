26200f3bf0deb4358f16c2efc174b633a926c8df
JKuriata <JK271096@outlook.com>
2024-02-06 11:12:19 +0000

    chore: cleanup

--- src/server.js
+++ src/server.js
@@ -3,12 +3,18 @@ const express = require("express");
 
 const app = express();
 
-app.use("/example", express.static("example"));
-app.use("/stormlight", express.static("stormlight"));
-
-// app.get("/health", (req, res) => {
-//     res.send("healthy");
-// });
+app.use("/book", (request, response) => {
+  const book = {
+    title: "Lord of the Rings",
+    author: "J.R.R Tolkien",
+    genre: "Fantasy",
+  };
+  const successResponse = {
+    message: "Book successfully retreived",
+    book: book,
+  };
+  response.send(successResponse);
+});
 
 app.listen(5001, () => {
   console.log("Server is listening on port 5001");
